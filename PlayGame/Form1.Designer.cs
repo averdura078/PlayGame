bc845cbc0fb3838b0280c1dcee81f68becd48693
averdura078 <averdura087@ed.amdsb.ca>
2024-09-11 11:18:38 -0400

    Add project files.

---
+++ PlayGame/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace PlayGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startButton = new System.Windows.Forms.Button();
+            this.countdownLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.startButton.FlatAppearance.BorderSize = 6;
+            this.startButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Red;
+            this.startButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Lime;
+            this.startButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.startButton.Font = new System.Drawing.Font("Microsoft Uighur", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.Location = new System.Drawing.Point(219, 173);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(348, 86);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "START GAME";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // countdownLabel
+            // 
+            this.countdownLabel.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.countdownLabel.Font = new System.Drawing.Font("Microsoft Uighur", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.countdownLabel.Location = new System.Drawing.Point(12, 9);
+            this.countdownLabel.Name = "countdownLabel";
+            this.countdownLabel.Size = new System.Drawing.Size(776, 432);
+            this.countdownLabel.TabIndex = 1;
+            this.countdownLabel.Text = "3...";
+            this.countdownLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightPink;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.countdownLabel);
+            this.Controls.Add(this.startButton);
+            this.Name = "Form1";
+            this.Text = "Start Game";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label countdownLabel;
+    }
+}
+
---
+++ PlayGame/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using System.Threading;
+
+namespace PlayGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            countdownLabel.Visible = false;
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            SoundPlayer sp = new SoundPlayer(Properties.Resources.startblipSound);
+            sp.Play();
+            Thread.Sleep(1500);
+            startButton.Visible = false;
+
+            countdownLabel.Visible = true;
+            sp.Play();
+            Refresh();
+            Thread.Sleep(1000);
+
+            countdownLabel.Text = "2...";
+            sp.Play();
+            Refresh();
+            Thread.Sleep(1000);
+
+            countdownLabel.Text = "1...";
+            sp.Play();
+            Refresh();
+            Thread.Sleep(1000);
+
+            countdownLabel.Text = "GO!!!";
+            countdownLabel.ForeColor = Color.White;
+            countdownLabel.BackColor = Color.Green;
+            //countdownLabel.Size = new Size(100, 100);
+            countdownLabel.Font = new Font("Showcard Gothic", 150, FontStyle.Bold);
+
+           
+            {
+                BackColor = Color.SpringGreen;
+                Thread.Sleep(100);
+                Refresh();
+                BackColor = Color.White;
+                Thread.Sleep(100);
+                Refresh();
+                //loop above six lines infinitely for visual effect
+            }
+
+            SoundPlayer sp2 = new SoundPlayer(Properties.Resources.gofanfareSound);
+            sp2.Play();
+        }
+    }
+}
